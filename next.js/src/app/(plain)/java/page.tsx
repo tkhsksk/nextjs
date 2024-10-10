c76188004348a534eb75849e584edbe88ec0b597
tkhsksk <tkhsksk0318@gmail.com>
2024-10-10 14:00:36 +0900

    template

--- next.js/src/app/(plain)/java/page.tsx
+++ next.js/src/app/(plain)/java/page.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from "next";
 // import Test from '@/app/common';
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { gml } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import Link  from 'next/link'
 import fs from 'fs'
+import Image from "next/image"
 
 export const metadata: Metadata = {
    title: 'java｜'+process.env.SITE_TITLE,
@@ -16,13 +18,11 @@ function Syntax(lang: string, code: string) {
    </div>;
 }
 
-function SyntaxCode(lang: string, code: string) {
-   const text  = fs.readFileSync("../"+lang+"/"+code+"."+lang+"", 'utf8')
+function SyntaxCode(lang: string, file: string) {
+   const text  = fs.readFileSync("../"+lang+"/"+file+"", 'utf8')
    const lines = text.toString().split('¥n')
    return <div className="grid">
-   <div className="grid overflow-hidden w-fit mt-6">
-   <p className="whitespace-nowrap overflow-x-auto text-base w-fill px-2 bg-slate-500 text-white">{code}.{lang}</p>
-   </div>
+   <div className="grid overflow-hidden w-fit mt-3"><p className="whitespace-nowrap overflow-x-auto text-base w-fill px-2 bg-slate-500 text-white">{file}</p></div>
    <SyntaxHighlighter language={lang} style={gml} className="mb-3">{lines}</SyntaxHighlighter>
    </div>;
 }
@@ -44,10 +44,13 @@ export default function Home() {
          <table className="text-left">
          <tbody className="dark:text-gray-600">
             <tr>
-               <th className="pr-4">java</th><td>openjdk version 23</td>
+               <th className="pr-4">java</th><td>v21.0.4</td>
             </tr>
             <tr>
-               <th className="pr-4">spring boot</th><td>v</td>
+               <th className="pr-4">Maven</th><td>v3.9.9</td>
+            </tr>
+            <tr>
+               <th className="pr-4">spring boot</th><td>v3.3.3</td>
             </tr>
             <tr>
                <th className="pr-4">mysql</th><td>v11.5.2-MariaDB</td>
@@ -66,15 +69,15 @@ export default function Home() {
             ファイルをコンパイルする必要がある<br />
             その手順を含めてHello World.を出力する方法を記載します<br />
             {WrapCode('java')}で文字列を表示する場合は、以下の表記で実行できます</p>
-            {SyntaxCode('java','Helloworld')}
+            {SyntaxCode('java','Helloworld.java')}
             <p className="leading-7 mb-3">{WrapCode('java')}の構文は{WrapCode('class')}および{WrapCode('public static void main(String[] args)')}が必要です<br />
             基本的にこの体系がないとコンパイルの時点でエラーが発生するのでエラーメッセージを元に確認しましょう<br />
             この.javaファイルに対して以下のコマンドを実行しコンパイルします</p>
-            {SyntaxCode('sh','javac')}
+            {SyntaxCode('sh','javac.sh')}
             <p className="mb-3">コンパイル実行後、{WrapCode('Helloworld.class')}ファイルが生成されます<br />
             .classファイルの中身は機械語のため、英語と数字の羅列があります<br />
             この.classファイルをさらに以下のコマンドで実行しましょう</p>
-            {SyntaxCode('sh','java')}
+            {SyntaxCode('sh','java.sh')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','Hello World.')}
             <p className="mb-3">改行については多言語と同じく{WrapCode('\\n')}を用います。</p>
@@ -84,13 +87,13 @@ export default function Home() {
             <h3 className="text-lg font-semibold mb-3">変数と型宣言</h3>
             <p className="leading-7">文字列にテキストや数値を格納しておくことも可能です<br />中学生の数学で学習したx=1,y=2と同じ要領です</p>
             <p className="font-semibold">実行内容</p>
-            {SyntaxCode('java','type_int')}
+            {SyntaxCode('java','type_int.java')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','hoge')}
             <p className="mb-3">{WrapCode('String x = "hoge";')}でxにhogeを格納し、それを{WrapCode('System.out.println()')}で呼び出します<br />
             文字を結合する場合にはx,yにそれぞれの文字列を格納して+(プラス)で結合することができます</p>
             <p className="font-semibold">実行内容</p>
-            {SyntaxCode('java','join')}
+            {SyntaxCode('java','join.java')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','hogepiyo')}
          </div>
@@ -104,17 +107,17 @@ export default function Home() {
          <div className="p-5 border-l-2 pe-0 sm:pe-5">
             <h3 className="text-lg font-semibold mb-3">計算</h3>
             <p className="leading-7">単純にプラスの符号を用いて、足し算をします</p>
-            {SyntaxCode('java','add')}
+            {SyntaxCode('java','add.java')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','16')}
             <p className="leading-7 mb-3">引き算、掛け算、割り算も以下の符号で計算可能です</p>
             <p className="font-semibold">実行内容</p>
-            {SyntaxCode('java','calc')}
+            {SyntaxCode('java','calc.java')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','6\n25\n2')}
             <p className="leading-7 mb-3">では、文字を{WrapCode('int')}型にせず{WrapCode('string')}型でプラス符号を使ったらどうなるでしょうか</p>
             <p className="font-semibold">実行内容</p>
-            {SyntaxCode('java','string')}
+            {SyntaxCode('java','string.java')}
             <p className="font-semibold">実行結果</p>
             {Syntax('shell','52')}
             <p>この場合{WrapCode('string')}で文字列として宣言しているため、足し算は行われず結合のみ行われます</p>
@@ -131,7 +134,7 @@ export default function Home() {
             <p className="leading-7 mb-3">{WrapCode('java')}ではデフォルトで{WrapCode('main')}関数が設定されているので、{WrapCode('main')}はそのまま宣言します<br />
             同じクラス内で他の関数を宣言し、そこから取得するパターンを試してみましょう<br />
             以下は別関数で足し算して、その関数結果を取得して表示しています</p>
-            {SyntaxCode('java','class_sub')}
+            {SyntaxCode('java','class_sub.java')}
             <p className="leading-7 mb-3">基本的な説明は、コード内にコメントアウトで表記していますが、{WrapCode('function')}のアクセス修飾子については以下に詳細を表記します</p>
 
             <div className="relative shadow-md sm:rounded-lg mb-4 grid overflow-hidden w-fit">
@@ -205,15 +208,15 @@ export default function Home() {
             javaでは{WrapCode("for")}と{WrapCode("forEach")}どちらも使えます<br />
             具体的には以下のように回します</p>
             <p className="font-semibold">forで配列を「回数制限して」回す</p>
-            {SyntaxCode('java','for')}
+            {SyntaxCode('java','for.java')}
             <p className="font-semibold">forで配列を「最後まで」回す</p>
-            {SyntaxCode('java','forEnd')}
+            {SyntaxCode('java','forEnd.java')}
 
             <hr className="my-5" />
 
             <p className="mb-3">次に{WrapCode('forEach')}で回すケースです<br />
             配列を宣言する場合もさまざまな宣言方法があるので、一挙宣言して回してみましょう</p>
-            {SyntaxCode('java','forEach')}
+            {SyntaxCode('java','forEach.java')}
             <p className="leading-7 mb-3">Mapsのみ引数を2つ用意する必要があるので、インデックスも取得しています<br />
             ところが、通常の配列を{WrapCode('forEach')}ではインデックスを取得できないとのことで<br />
             通常の配列でインデックスを使う場合は、for文で回す必要があるようです</p>
@@ -224,7 +227,7 @@ export default function Home() {
             <p className="leading-7 mb-3">一つの要素に複数の情報を含めたいときに利用する多次元配列を利用する場合です<br />
             この場合以下のように設定し、取得します</p>
             <p className="font-semibold">多次元配列</p>
-            {SyntaxCode('java','forEachIn')}
+            {SyntaxCode('java','forEachIn.java')}
             <p className="leading-7 mb-3">本来は{WrapCode('{0,"test"}')}として、インデックスは{WrapCode('int')}で取得したいところですが、<br />
             多次元配列に複数の型を宣言できないようなので、{WrapCode('String')}で数字を入力しています</p>
 
@@ -234,15 +237,15 @@ export default function Home() {
             <p className="leading-7 mb-3">配列から配列を作成することで、簡易的なデータ検索が可能です<br />
             配列を回して、その中で該当となるデータのみ格納する配列を新規に作成すると、検索済みデータの配列を作成できます</p>
             <p>まずは{WrapCode('if')}で表示するのみの場合</p>
-            {SyntaxCode('java','IfElse')}
+            {SyntaxCode('java','IfElse.java')}
             <p>また新規にデータ配列を作成する場合は、空の配列を作成して、{WrapCode('for文')}と{WrapCode('if文')}で配列に格納していきます</p>
-            {SyntaxCode('java','Search')}
+            {SyntaxCode('java','Search.java')}
             <p>新規に検索データの配列を作成する場合は{WrapCode('for')}よりも、<br />
             繰り返しの処理に向いている、{WrapCode('forEach')}の方が分かりやすく且つ作りやすい構造です<br />
             ただしインデックスを付けたい場合などの場合は、{WrapCode('Map')}や{WrapCode('for文')}を使うなど工夫しなければなりません<br />
             また配列の作成は前述にもあるとおり複数あるので、ある程度自分の中で特定の{WrapCode('import')}などを確定させる必要があります<br /><br />
             繰り返しの処理で言うと、例えばすべてのデータに🐶をつける場合も</p>
-            {SyntaxCode('java','Dog')}
+            {SyntaxCode('java','Dog.java')}
             <p>で可能です<br />
             {WrapCode('java')}ではやはり複数の方法があるため、逆に分かりづらい部分も多そうです</p>
 
@@ -258,10 +261,10 @@ export default function Home() {
             <p className="font-semibold mb-3">CRUDを始める前に</p>
             <p className="leading-7 mb-3">前提としてmysqlにdatabase名{WrapCode('java')}を作成、user(guest)を作成し、guestに対して{WrapCode('java')}へのすべての権限を付与しています<br />
             データベース{WrapCode('java')}には{WrapCode('toy')}というテーブルを作成し、id,nameをカラムとして登録しました</p>
-            {SyntaxCode('sh','db_java')}
+            {SyntaxCode('sh','db_java.sh')}
             <p className="leading-7 mb-3">また、db操作するためには{WrapCode('jdbcコネクタ')}が別途必要となるため、{WrapCode('mysql-connector-j-9.0.0.jar')}をダウンロードし<br />
             {WrapCode('lib')}フォルダに移動させてクラスパスを通す必要があります</p>
-            {SyntaxCode('sh','use_jdbc')}
+            {SyntaxCode('sh','use_jdbc.sh')}
             <p className="leading-7 mb-3">ここまででようやく{WrapCode('CRUD')}の操作が可能となります</p>
 
             <hr className="my-5" />
@@ -273,10 +276,10 @@ export default function Home() {
             {WrapCode('insert')}には各種方法がありますが、mysqliにて{WrapCode('insert')}する例を示します<br />
             これまでhostは{WrapCode('localhost')}などになっていましたが、javaの場合は{WrapCode('jdbc')}を利用するため{WrapCode('jdbc:mysql://localhost/java')}と指定します、ここだけ多言語とは違う部分です<br />
             データベース{WrapCode('java')}、テーブル{WrapCode('toy')}に{WrapCode('id:1、name:bear')}をinsertするjavaが以下です</p>
-            {SyntaxCode('java','insert')}
+            {SyntaxCode('java','insert.java')}
             <p>実際に登録されたかどうかを確認するために<br />
             データを読み込む、readするjavaを作成する前に、shell上でコマンドを利用し中身を見てみましょう</p>
-            {SyntaxCode('sh','insert_result_java')}
+            {SyntaxCode('sh','insert_result_java.sh')}
 
             <hr className="my-5" />
 
@@ -286,7 +289,7 @@ export default function Home() {
             ですが実際に各種サイトデータを利用する場合は、登録後にread(読み込み)し出力させなければなりません</p>
             <p className="mb-3">それではjavaから{WrapCode('select')}を使って出力してみます<br />
             そのままdbのデータを表示させることも可能ですが、今回はdbのデータを配列に格納し、その配列を回して表示させるパターンも記載します</p>
-            {SyntaxCode('java','read')}
+            {SyntaxCode('java','read.java')}
             <p>配列に格納しておくと、別コントローラにデータを渡したり、検索結果を表示させるときに有効です</p>
 
             <hr className="my-5" />
@@ -294,9 +297,9 @@ export default function Home() {
             <p className="font-semibold mb-3">Update</p>
             <p className="mb-3">dbに登録したデータを変更したい場合もあるかと思います、その場合は{WrapCode('update')}を使ってデータを更新します<br />
             先ほど{WrapCode('insert')}したデータをbear→robotに{WrapCode('update')}してみましょう</p>
-            {SyntaxCode('java','update')}
+            {SyntaxCode('java','update.java')}
             <p>実際に更新されたことを確認しましょう</p>
-            {SyntaxCode('sh','update_result_java')}
+            {SyntaxCode('sh','update_result_java.sh')}
 
             <hr className="my-5" />
 
@@ -305,9 +308,9 @@ export default function Home() {
             そこに公開非公開をフラッグとすることが多いです<br />
             特にdbからデータを削除してしまうと二度とデータを閲覧できなくなってしまいます<br />
             そのため、deleteするケースは少ないですが、deleteする場合は{WrapCode('delete')}を使ってデータを更新します</p>
-            {SyntaxCode('java','delete')}
+            {SyntaxCode('java','delete.java')}
             <p>実際に削除されたことを確認しましょう</p>
-            {SyntaxCode('sh','delete_result_java')}
+            {SyntaxCode('sh','delete_result_java.sh')}
          </div>
 
       </section>
@@ -323,15 +326,61 @@ export default function Home() {
             ページロード時に消えてしまう{WrapCode('POST')}に対して、{WrapCode('GET')}はurl上のパラメータを取得するため、反恒久的にデータを保持することが可能です<br />
             {WrapCode('POST,GET')}も実行できるjavaを作成する予定でしたが<br />
             htmlやTomcatも関わってくるため、実行できるはずのプログラムを以下に示します</p>
-            {SyntaxCode('html','java_form')}
-            {SyntaxCode('java','post')}
+            {SyntaxCode('html','java_form.html')}
 
             <hr className="my-5" />
 
             <p className="font-semibold mb-3">GET</p>
             <p className="leading-7 mb-3">クエリパラメータを取得する場合です<br />
             urlの／以降が{WrapCode('?name=hoge')}の場合、{WrapCode('hoge')}という実行結果が表示されるかと思います</p>
-            {SyntaxCode('java','get')}
+            {SyntaxCode('java','get.java')}
+         </div>
+
+      </section>
+
+      <section id="Spring_Boot">
+         <h2 className="text-2xl font-semibold mb-3">Spring Boot</h2>
+         <p className="leading-7 mb-3">javaの代表的なフレームワークSpring Bootをインストールします<br />
+         インストールの前にJavaをインストールしましょう</p>
+
+         <div className="p-5 border-l-2 pe-0 sm:pe-5">
+            <p className="font-semibold mb-3">環境構築</p>
+            <p className="leading-7 mb-3">Javaをインストール</p>
+            {SyntaxCode('java','spring/ec2/install.sh')}
+            <p className="leading-7">ここまでで環境は整いました<br />
+            <Link href="http://34.197.33.76:8080/" target="_blank">http://34.197.33.76:8080/</Link><br />
+            ローカルでも同じように動作するように各種インストールを行いましょう<br />
+            gitも用意し、ローカルで編集できるようにします<br /><br />
+            git cloneしたものをローカルで実行してみましょう</p>
+            {SyntaxCode('java','spring/macOS/install.sh')}
+            <p className="leading-7"><Link href="http://localhost:8080/" target="_blank">http://localhost:8080/</Link>でページ表示が確認できます</p>
+
+            <hr className="my-5" />
+
+            <p className="font-semibold mb-3">ページ表示</p>
+            <p className="leading-7">ページを表示させます</p>
+
+            {SyntaxCode('java','spring/macOS/root_1.sh')}
+            {SyntaxCode('java','spring/template/pom.xml')}
+            {SyntaxCode('java','spring/macOS/root_2.sh')}
+            {SyntaxCode('java','spring/template/index.html')}
+            {SyntaxCode('java','spring/template/HomeController.java')}
+            <p className="leading-7 mb-3"><Link href="http://localhost:8080/" target="_blank">http://localhost:8080/</Link></p>
+            <Image
+              className="mb-3"
+              src={'/java/template.png'}
+              alt="success"
+              width={280}
+              height={38}
+              priority
+            />
+
+            <hr className="my-5" />
+
+            <p className="font-semibold mb-3">データ登録</p>
+            <p className="leading-7">ページを表示させます</p>
+
+            {SyntaxCode('java','spring/macOS/db.sh')}
          </div>
 
       </section>
